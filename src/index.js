b0ef4abc4b5cce42219da108e587f4ec77a567f1
Mudassir Malik <mdasirmalik11@gmail.com>
2025-02-12 21:59:20 +0500

    utility setting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // require ('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 
-// console.log("MONGODB_URI:", process.env.MONGODB_URI); // Debugging line
 dotenv.config({path:'./env'})
 import connectDb from "./db/index.js";
 
 
 connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 800, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch( (err) => {
+console.log(`Database connection failed !!!`,err)
+})
